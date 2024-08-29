2044aeb9612b3a07bc9d831c2858c39f7cf8d782
FireJ7 <163463357+FireJ7@users.noreply.github.com>
2024-08-29 00:17:20 -0400

    prueba2

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -16,6 +16,7 @@ public class Prueba {
     public static void main(String[] args) 
     {   
         System.out.println("HOla mundo");
+        System.out.println("HOla mundo");
         // TODO code application logic here
     }
     
